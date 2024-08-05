5b5460b3c61dcd714a379700050c45073f9eb216
Adams Kingsley <Yelsgink@gmail.com>
2024-08-05 10:54:27 +0100

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,28 @@
+import Link from "next/link";
+
+//Icons
+import {
+    RiYoutubeLine,
+    RiInstagramLine,
+    RiGithubLine,
+    RiTwitterLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-    return <div>Socials</div>;
+    return <div className="flex items-center gap-x-5">
+        <Link href={''} className="hover:text-accent transition-all duration-300">
+            <RiGithubLine />
+        </Link>
+        <Link href={''} className="hover:text-accent transition-all duration-300">
+            <RiTwitterLine />
+        </Link>
+        <Link href={''} className="hover:text-accent transition-all duration-300">
+            <RiInstagramLine />
+        </Link>
+        <Link href={''} className="hover:text-accent transition-all duration-300">
+            <RiYoutubeLine />
+        </Link>
+    </div>;
 };
 
 export default Socials;
