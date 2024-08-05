19e58e5fb2229669e96b0b815456b6dc18b97472
Adams Kingsley <Yelsgink@gmail.com>
2024-08-05 02:51:40 +0100

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const Avatar = () => {
-    return <div>Avatar</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="relative w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]">
+        <Image
+          src="/avatar.png"
+          alt="Avatar"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Avatar;
