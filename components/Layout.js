054fba0d37fdedc9baea166315ec1cf2648c652a
Adams Kingsley <Yelsgink@gmail.com>
2024-08-05 02:51:52 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,30 @@
 // fonts
-import { Sora } from '@next/font/google'
+import { Sora } from 'next/font/google';
 
 // font settings
 const sora = Sora({
     subsets: ['latin'],
     variable: '--font-sora',
-    weight: ['100', '200', '300', '400', '500', '600', '700', '800']
-})
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
 
-//components
-import Nav from '../components/Nav'
-import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
+// components
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLeftImg from '../components/TopLeftImg';
 
 const Layout = ({ children }) => {
-    return <div className={'page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative'}
-    >
-        <TopLeftImg />
-        <Nav />
-        <Header />
-        {children}
-    </div>;
+    return (
+        <div
+            className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+            style={{ fontFamily: 'var(--font-sora)' }}
+        >
+            <TopLeftImg />
+            <Nav />
+            <Header />
+            {children}
+        </div>
+    );
 };
 
 export default Layout;
