3c351e8b5199dfb026fd11694d617c442fb53cb0
Adams Kingsley <Yelsgink@gmail.com>
2026-04-12 05:22:52 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,18 +8,18 @@ const Header = () => {
             <div className="container mx-auto">
                 <div className="flex flex-row justify-between items-center py-4 xl:py-8">
                     {/* logo */}
-                    <Link href={'/'}>
-                        <div className="relative w-[120px] h-[33px] xl:w-[160px] xl:h-[44px]">
-                            <Image
-    src={'/logo.png'}
-    alt="Logo"
-    fill
-    style={{ objectFit: 'contain', objectPosition: 'left center' }}
-    priority={true}
-/>
-                        </div>
-                    </Link>
-                    {/* socials */}
+                                        <Link href={'/'}>
+                                            <div className="relative w-[140px] h-[38px] xl:w-[210px] xl:h-[70px]">
+                                                <Image
+                                                    src={'/logo.png'}
+                                                    alt="Logo"
+                                                    fill
+                                                    style={{ objectFit: 'contain', objectPosition: 'left center' }}
+                                                    priority={true}
+                                                />
+                                            </div>
+                                        </Link>
+                                        {/* socials */}
                     <Socials />
                 </div>
             </div>
